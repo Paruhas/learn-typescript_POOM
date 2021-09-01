4c5f055e405b8ae163e7c3f7963b715f88814748
Paruhas <bayulgang@hotmail.com>
2021-09-01 22:51:24 +0700

    first commit 15.05

---
+++ note/29-08-64.js
@@ -0,0 +1,30 @@
+const add1 = (a, b) => a + b;
+
+/**
+ * @param a{number}
+ * @param b{number}
+ * @returns(number)
+ */
+
+const add2 = (a, b) => {
+  return a + b;
+};
+
+console.log(add2("1", 1));
+
+/* ========================== */
+
+const person1 = {
+  nickname: "BOB",
+  age: 15,
+};
+
+/**
+ * @type {{nickname: string}}
+ */
+
+// const nickname = "Bob"; // ไว้ตอนจะ node ไฟล์ person2 จะได้ไม่ error
+
+const person2 = {
+  nickname,
+};
