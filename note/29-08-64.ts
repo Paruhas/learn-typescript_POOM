3bae77b8d8626bb2285de1eaa6e186096cfb5014
Paruhas <bayulgang@hotmail.com>
2021-09-26 05:00:28 +0700

    03.07.57

--- note/29-08-64.ts
+++ note/29-08-64.ts
@@ -196,3 +196,317 @@ type TAngle = 0 | 90 | 180 | 270 | 360;
 function setAngles(angle: TAngle) {}
 
 setAngles(90);
+
+// Union and Intersect
+type IFirstName = {
+  fname: string;
+};
+
+type ILastName = {
+  lname: string;
+};
+
+function getFullNameUnion(name: IFirstName | ILastName) {
+  name;
+}
+
+function getFullNameIntersect(name: IFirstName & ILastName) {
+  name.fname;
+}
+
+// Discriminated Unions
+
+type User = {
+  name: string;
+};
+
+type Action =
+  | { type: "setUser"; payload: User }
+  | { type: "getUser"; payload: undefined; error: Error };
+
+function reducer(state: User, action: Action) {
+  action.type; // ' type: "setUser" | type: "getUser" '
+
+  if (action.type === "setUser") {
+    action.payload; // ' payload: User '
+  }
+}
+
+// Shaping the data model with Interface and Type ( and Union )
+// Union VS Intersect
+
+type Hello1 = { name: string } & { name: number };
+type Hello2 = { name: string } | { name: number };
+
+const hello1: Hello2 = { name: "a" };
+type StringAndNumber = string & number;
+
+// Optional
+
+function fetchUserData(userId: number, context?: unknown) {}
+
+fetchUserData(1);
+
+// Intersect VS extends
+
+type A1 = string & number;
+type A2 = string & "Hello";
+
+interface PermissionObj {
+  name: string;
+}
+
+// interface User1 extends PermissionObj {
+//   name: number;
+// }
+
+interface Person2 {
+  username: string;
+  age?: number;
+  status?: string;
+}
+
+const person6: Person2 = { username: "abc" };
+
+// Type Casting: As and As Unknown
+
+function loadFeed(post: (string | number)[]) {
+  const postAs = post as unknown as {};
+  postAs;
+}
+
+loadFeed([1, 2, 3, "4"]);
+
+// Enums vs Enums with String Values
+
+enum Version {
+  /** Version1 */
+  v1 = "v1",
+  v2 = "v2",
+}
+
+function setVersion(version: Version) {
+  if (version === Version.v1) {
+    version;
+  }
+}
+
+// as const
+
+const pie = ["22/7", "3.14"] as const;
+type K = typeof pie[number];
+
+// pie.push() // Error
+
+// Type Narrowing and Widening
+const isYed = (name: string): name is "yed" => true;
+
+function sayYed(name: string) {
+  if (name === "yed") {
+    name;
+  }
+
+  if (isYed(name)) {
+    name;
+  }
+}
+
+// Generic Types
+
+function toggleNormal(a: string | number): string | number {
+  return a;
+}
+
+const resultToggleNormal = toggleNormal("hello");
+
+function toggleGeneric<T extends string | number>(a: T): T {
+  return a;
+}
+
+const resultToggleGeneric1 = toggleGeneric("hello");
+const resultToggleGeneric2 = toggleGeneric(1);
+// const resultToggleGeneric3 = toggleGeneric(true); // Error
+
+// Generic Keyof
+
+interface Book {
+  id: string;
+  isbn: string;
+}
+
+type keyOfBook = keyof Book;
+var example: keyof Book = "id"; // will auto correct `id` or `isbn` (Union) [key name in Interface Book]
+type valueOfBook = Book["id"]; // will auto correct `id` or `isbn` (Union) [key name in Interface Book]
+
+function setBook<K extends keyof Book, V extends Book[K]>(key: K, value: V) {}
+
+setBook("id", "1");
+setBook("isbn", "ABP-255");
+
+type A1B2 = { a: 1; b: 2 };
+type A1B2_C = keyof A1B2;
+
+type A1B2_editKey = `-${A1B2_C}`;
+
+function tests<K extends A1B2_C, V extends A1B2[K]>(key: K, value: V) {}
+
+tests("a", 1);
+tests("b", 2);
+
+// Using Generics
+
+function sampleType(value: string | number): string | number {
+  return value;
+}
+
+const getSampleType = sampleType(20); // getSampleType = string | number
+
+function genericsType<T extends string | number>(value: T): T {
+  return value;
+}
+
+const getGenericsType = genericsType(20); // getGenericsType = 20
+
+// Map over types -> Mapped Type
+
+type Statuses = "active" | "inactive";
+
+type Statuses1 = {
+  [P in "active" | "inactive"]: boolean;
+};
+
+// Non-Null Assertion Operator (!)
+
+function getSomething(): null | string {
+  return null;
+}
+
+const something = getSomething()!;
+something?.concat();
+
+// Common TS Helper Types (Generics) & How to read generics
+// Record, Partial, Readonly, Omit, Pick, Exclude, etc.
+//Record
+
+type Record_Sample<K extends keyof any, T> = {
+  [P in K]: T;
+};
+
+const universityStudentAge: Record<string | number, number> = {
+  Y1: 18,
+  20: 20,
+};
+
+type Record2 = {
+  [P in string | number]: number;
+};
+
+const recordStatus: Record2 = {
+  Y2: 19,
+  21: 21,
+};
+
+type Record3<K extends string, V> = {
+  [P in K]: V;
+};
+
+type Record3_Status = Record3<Statuses, boolean>;
+
+//Partial
+
+interface LanguageCode {
+  en_US: string;
+  th_TH: string;
+}
+
+type OptionLanguageCode = Partial<LanguageCode>;
+
+const languageCodeMap: OptionLanguageCode = {};
+
+type Partial_Sample<T> = {
+  [P in keyof T]?: T[P];
+};
+
+//Readonly
+
+type SeverID = {
+  id: number;
+};
+
+type Readonly_Sample<T> = {
+  readonly [P in keyof T]: T[P];
+};
+
+type ReadonlyServerID = Readonly_Sample<SeverID>;
+
+//Pick
+
+type Pick_Sample<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+interface Book1 {
+  id: number;
+  name: string;
+  description: string;
+}
+
+type Book1_ThatHaveSomeData = Pick<Book1, "id" | "name">;
+
+const newBook1: Book1_ThatHaveSomeData = {
+  id: 2,
+  name: "YES",
+};
+
+//Exclude, Extract
+
+type UnluckyNumber = 4 | 13 | 112 | 555;
+
+type UnluckyNumber_Without555 = Exclude<UnluckyNumber, 555>;
+type UnluckyNumber_Real = Extract<UnluckyNumber, 0 | 4 | 13>;
+
+// Readonly in Interface
+
+//Conditional Type
+
+type CheckType<T> = T extends string
+  ? "is String"
+  : T extends number
+  ? "is Number"
+  : "none at all";
+
+type ResultCheck_string = CheckType<"a">;
+type ResultCheck_number = CheckType<1>;
+type ResultCheck_none = CheckType<true>;
+
+//Infer
+
+type CheckTypeInfer1<T> = T extends Array<infer InsideArr>
+  ? ["is Array", InsideArr]
+  : "none at all";
+
+type ResultCheckInfer1_array1 = CheckTypeInfer1<[50]>;
+type ResultCheckInfer1_array2 = CheckTypeInfer1<[50, 30]>;
+
+type CheckTypeInfer2<T> = T extends [infer index00, infer index01]
+  ? ["is Array", index00, index01]
+  : "none at all";
+
+type ResultCheckInfer2_array1 = CheckTypeInfer2<[50]>;
+type ResultCheckInfer2_array2 = CheckTypeInfer2<[50, 30]>;
+
+type CheckTypeInfer_Long<T> = T extends [infer index00, ...infer indexXX]
+  ? ["is Array", index00, indexXX]
+  : "none at all";
+
+type ResultCheckInfer_Long_array1 = CheckTypeInfer_Long<[50]>;
+type ResultCheckInfer_Long_array2 = CheckTypeInfer_Long<[50, 30]>;
+
+// Additional Content
+//Creat JSON typescript Schema
+
+function ref<T>(type: T) {
+  return { type: "ref" as "ref", value: type };
+}
+
+const newRef = ref({ type: "number" as "number" });
